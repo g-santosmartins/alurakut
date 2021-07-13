7e186d3b5e9646146f695ba2f4987f0e2bda35e4
Guilherme Martins <g.santosmartins@outlook.com>
2021-07-13 13:06:15 -0300

    Desacoplamento do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box >
+         <img src="https://github.com/g-santosmartins.png"/>
+        </Box>
+      </div>
+
+      <div className="welcomeArea"  style={{ gridArea: 'welcomeArea' }}>
+        <Box >
+          Bem vindo
+        </Box>
+      </div>
+
+      <div className="profileRelations"  style={{ gridArea: 'profileRelations' }}>
+        <Box >
+          Pessoas da comunidade
+          Comunidade
+        </Box>
+      </div>
+
+    </MainGrid>
+  )
 }
