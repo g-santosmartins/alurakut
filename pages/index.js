96947cdb695f81a74a56270c5f6bcae95deca8b7
Guilherme Martins <g.santosmartins@outlook.com>
2021-07-14 10:54:26 -0300

    inicio segundo dia dia de imersão

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
-import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+import { ProfileRelationsBoxWrapper, AlurakutProfileSidebarMenuDefault } from '../src/components/ProfileRelations'
 
 // function component
 function ProfileSidebar(props) {
